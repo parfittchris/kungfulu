2e260ddf9587152e224e692acea00eba2dabce7a
Chris Parfitt <parfitt.chris7@gmail.com>
2019-07-30 18:08:30 -0400

    fix user error

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,11 +1,11 @@
 class Api::SessionsController < ApplicationController
     def create
         @user = User.find_by_credentials(params([user][username]), params([user][password]))
-        if @user.nil? 
-            render json: "Can't find user"
-        else
+        if @user 
             login(@user)
             render "api/users/show"
+        else
+            render json: "Can't find user"
         end
     end
 
