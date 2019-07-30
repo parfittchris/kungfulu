868e172ed836c86bee5fe0952c5a7d2e72995c7f
Chris Parfitt <parfitt.chris7@gmail.com>
2019-07-30 18:14:45 -0400

    fix ruby error

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,11 +1,13 @@
 class Api::SessionsController < ApplicationController
     def create
         @user = User.find_by_credentials(params([user][username]), params([user][password]))
-        if @user 
-            login(@user)
-            render "api/users/show"
+
+        if @user.nil?
+            flash.now[:errors] = ['Invalid username or password.']
+            render :new
         else
-            render json: "Can't find user"
+            login!(@user)
+            redirect_to user_url(@user)
         end
     end
 
