6d93f77e186868a9b0b59d5cdebb3c23e27735b0
Chris Parfitt <parfitt.chris7@gmail.com>
2019-08-07 08:26:58 -0400

    fix heroku error 1

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,11 +2,11 @@ class Api::SessionsController < ApplicationController
     def create
         @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
             
-        if @user.nil?
-            render json: ["Invalid username/password combination"], status: 401
-        else
+        if @user
             login!(@user)
             render "api/users/show"
+        else
+            render json: ["Invalid username/password combination"], status: 401
         end
     end
 
